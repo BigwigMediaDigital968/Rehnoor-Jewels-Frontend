37e66892602fc2992ddd1afdb8e103966aac6f62
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-04-18 11:01:14 +0530

    updated code for tracking bug

---
+++ app/component/website/Loader.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Loader = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isLoading) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
+  );
+};
+
+export default Loader;
