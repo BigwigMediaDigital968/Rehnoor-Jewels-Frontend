ed347b5b9ae55a507494cd1b8e61d4055f18f515
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-07-17 12:39:33 +0530

    meta pixel removal and new ctm code

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,6 +100,14 @@ export default function RootLayout({
     `}
         </Script> */}
 
+        {/* <!-- Google Tag Manager --> */}
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TLBLWL73');`}</script>
+        {/* <!-- End Google Tag Manager --> */}
+
         {/* Microsoft Clarity */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
@@ -112,7 +120,7 @@ export default function RootLayout({
         </Script>
         {/* <!-- Meta Pixel Code --> */}
 
-<Script id="facebook-pixel" strategy="afterInteractive">
+        {/* <Script id="facebook-pixel" strategy="afterInteractive">
           {`
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -125,7 +133,7 @@ export default function RootLayout({
           fbq('init', '1037782932526589');
           fbq('track', 'PageView');
           `}
-        </Script>
+        </Script> */}
         <noscript>
           <img
             height="1"
